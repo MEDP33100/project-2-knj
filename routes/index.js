2308b718905f5bcefa37a7ecc779854586edacb9
ReahKy23 <Kylah.kerr23@gmail.com>
2025-04-15 16:25:38 -0400

    test commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Crave NYC 🥡',
+    tagline: 'Outdoor tables, local flavors - perfect for you, the crew, and fido too'
+  });
 });
 
 module.exports = router;
