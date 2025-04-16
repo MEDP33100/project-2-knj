e520c9479e49b694d632b72668d530654f4739aa
ReahKy23 <Kylah.kerr23@gmail.com>
2025-04-15 20:30:50 -0400

    updated form
    Just some cosmetic changes made. A "restart" button created.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Crave NYC 🥡',
     tagline: 'Outdoor tables, local flavors - perfect for you, the crew, and fido too',
-    search: 'Start your grub search by completing the form below'
+    search: 'Start your grub search by completing the form below',
+    results: 'These are you results based on your search for __________ cuisine',
   });
 });
 
